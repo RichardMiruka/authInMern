16318b1043eb895132095fc6d14070615e95829c
RichardMiruka <richardonsare15@gmail.com>
2024-10-30 14:57:37 +0300

    project initialization and dependancy installation

---
+++ server/index.js
@@ -0,0 +1,11 @@
+ require("dotenv").config();
+ const express = require("express");
+ const cors = require("cors");
+ const app = express();
+
+ // middlewares
+ app.use(express.json());
+ app.use(cors());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
