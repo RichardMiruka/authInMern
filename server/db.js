91b05145717ec28bac61d15a876135a4ce0568b0
RichardMiruka <richardonsare15@gmail.com>
2024-10-31 16:29:50 +0300

    mongodb connection string created

--- server/db.js
+++ server/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    };
     try {
-        mongoose.connect(process.env.DB, connectionParams);
+        mongoose.connect(process.env.DB); // Removed deprecated options
         console.log("connected to database successfully");
     } catch (error) {
         console.log(error);
         console.log("could not connect to database");
     }
-}
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
  require("dotenv").config();
  const express = require("express");
- const cors = require("cors");
  const app = express();
+ const cors = require("cors");
+ const connection = require("./db");
+
+ // databse connection
+ connection();
+ 
 
  // middlewares
  app.use(express.json());
