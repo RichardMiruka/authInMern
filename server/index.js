4788298b560aaaf392f8830b3c123e2dbab33b6b
RichardMiruka <richardonsare15@gmail.com>
2024-11-01 15:05:00 +0300

    react project initialization and dependancy installation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@
  const app = express();
  const cors = require("cors");
  const connection = require("./db");
+ const userRoutes = require("./routes/users");
+ const authRoutes = require("./routes/auth");
 
  // databse connection
  connection();
@@ -11,6 +13,10 @@
  // middlewares
  app.use(express.json());
  app.use(cors());
+
+ // routes
+ app.use("/api/users", userRoutes);
+ app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
